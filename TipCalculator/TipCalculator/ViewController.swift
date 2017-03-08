34448e0dec71374c6c34121e9896d9573b0314ca
djq99 <jduan4@dons.usfca.edu>
2017-03-07 17:12:17 -0800

    add life cycle test

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -15,6 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var percentage: UISegmentedControl!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
